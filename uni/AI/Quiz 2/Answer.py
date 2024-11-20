c8a8588d252c230ba0e5074a3da41968041236f4
script <mmoatazmm8@gmail.com>
2024-11-20 03:40:57 +0200

    .

---
+++ uni/AI/Quiz 2/Answer.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+Heart_df = pd.read_csv("heart_disease_uci.csv")
+
+numerical = Heart_df.select_dtypes(include=["int64", "float64"]).columns
+categorical = Heart_df.select_dtypes(include=["object"]).columns
+
+for col in categorical:
+    le = LabelEncoder()
+    Heart_df[col] = Heart_df[col].astype(str)
+    Heart_df[col] = le.fit_transform(Heart_df[col])
+
+scaler = MinMaxScaler()
+Heart_df[numerical] = scaler.fit_transform(Heart_df[numerical])
+
+correlation_matrix = Heart_df.corr()
+plt.figure(figsize=(12, 8))
+
+sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", cbar=True, square=True)
+plt.title("Heart Disease")
+plt.show()
+
+correlation_matrix = correlation_matrix.abs()
+
+upperTri = np.triu(np.ones(correlation_matrix.shape), k=1).astype(np.bool)
+
+correlation_matrix = correlation_matrix.where(upperTri)
+
+redundant = []
+for column in correlation_matrix.columns:
+    if any(correlation_matrix[column] > 0.95):
+        redundant.append(column)
+
+
+Heart_df.drop(redundant, axis=1, inplace=True)
+
+print(Heart_df)
--- uni/AI/Quiz 2/lab5.py
+++ uni/AI/Quiz 2/lab5.py
@@ -1,23 +1,36 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.preprocessing import LabelEncoder
+import numpy
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
 df = pd.read_csv("heart_disease_uci.csv")
-# df = df.dropna().reset_index(drop=True)
+df = df.dropna().reset_index(drop=True)
 
+
+columnnames = df.columns.tolist()
+new = pd.DataFrame()
+
+for i in range(16):
+    target = df.iloc[:, i]
+    # print(type(df.iloc[1, i]))
+    if type(df.iloc[1, i]) in {numpy.float64, numpy.int64}:
+        target = MinMaxScaler().fit_transform(target)
+        target = pd.DataFrame(target, columns=[columnnames[i]])
+    new = pd.concat([new, target], axis=1)
+
+df = new
 
 new = df.iloc[:, 0]
-columnnames = df.columns.tolist()
 for i in range(1, 16):
     target = df.iloc[:, i]
     if type(df.iloc[1, i]) == str:
         target = LabelEncoder().fit_transform(target)
         target = pd.DataFrame(target, columns=[columnnames[i]])
-
     new = pd.concat([new, target], axis=1)
 
-print(new)
+
+# print(new)
 
 # correlation_matrix = df.corr()
 # plt.figure(figsize=(10, 8))
